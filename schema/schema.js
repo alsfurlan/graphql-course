619cff63da3cc4098ce8de97da9b468aa7328680
Anderson Luís Furlan <alsfurlan@gmail.com>
2023-10-17 19:06:44 -0300

    Nested Queries

--- schema/schema.js
+++ schema/schema.js
@@ -3,18 +3,22 @@ const graphql = require("graphql");
 const { GraphQLObjectType, GraphQLString, GraphQLInt, GraphQLSchema } = graphql;
 const axios = require("axios");
 
+const CompanyType = new GraphQLObjectType({
+  name: "Company",
+  fields: {
+    id: { type: GraphQLString },
+    name: { type: GraphQLString },
+    description: { type: GraphQLString },
+  },
+});
+
 const UserType = new GraphQLObjectType({
   name: "User",
   fields: {
-    id: {
-      type: GraphQLString,
-    },
-    firstName: {
-      type: GraphQLString,
-    },
-    age: {
-      type: GraphQLInt,
-    },
+    id: { type: GraphQLString },
+    firstName: { type: GraphQLString },
+    age: { type: GraphQLInt },
+    company: { type: CompanyType },
   },
 });
 
@@ -26,11 +30,12 @@ const RootQuery = new GraphQLObjectType({
       args: {
         id: {
           type: GraphQLString,
-        }, 
+        },
       },
       resolve(parentValue, args) {
-        return axios.get(`http://localhost:3000/users/${args.id}`)
-            .then(response => response.data);
+        return axios
+          .get(`http://localhost:3000/users/${args.id}`)
+          .then((response) => response.data);
       },
     },
   },
